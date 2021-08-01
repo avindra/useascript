377f184f5744ad0f623f2866f487266a85103198
Avindra Goolcharan <aavindraa@gmail.com>
2021-08-01 18:12:22 -0400

    twitter.com: hook navigation as well
    Turns out the entire app gets repainted, likely
    due to mishandling of React keys.
    
    So we'll just watch the history and reapply
    the fix as needed.
    
    This also adds the ability to toggle the node
    back on and off.

--- twitter.com/index.js
+++ twitter.com/index.js
@@ -1,64 +1,78 @@
-let maxAttempts = 100, n = 0;
+const doIt = () => {
+	let maxAttempts = 100, n = 0, hidden = true;
 
 
-/**
- * 
- * @param {Function} callback 
- * @returns {MutationCallback}
- */
-const createObserver = (callback) => {
 	/**
 	 * 
-	 * @param {MutationRecord[]} list
+	 * @param {Function} callback 
+	 * @returns {MutationCallback}
 	 */
-	const mutationCb = (list) => {
-		for(const a of list) {
-			a.target.display = 'none';
-		}
-	};
-
-	return mutationCb;
-}
-
-
-/**
- * on Twitter.com: hide the "What's happening"
- * panel by default. A lot of fear-mongering
- * and garbage gets pushed here.
- *
- * Users can click the header to expand it
- * on demand.
- */
-const checker = () => {
-	// safeguard in case user is not on main Twitter app
-	if (++n >= maxAttempts) clearInterval(pid);
-
-	const result = document.evaluate(`//span[text()="What’s happening"]/../../../../..`, document, null, 9, null);
-	const fakeNews = result.singleNodeValue;
-	if (fakeNews) {
-		clearInterval(pid);
-		// ready to work
-		const anchor = fakeNews.childNodes[1];
-		anchor.onclick = () => {
-			let fakeStory = anchor;
-			while (fakeStory = fakeStory.nextElementSibling) {
-				fakeStory.style.display = '';
+	const createObserver = (callback) => {
+		/**
+		 * 
+		 * @param {MutationRecord[]} list
+		 */
+		const mutationCb = (list) => {
+			for(const a of list) {
+				a.target.display = 'none';
 			}
 		};
 
-		const hideFakeNews = () => {
-			let fakeStory = anchor;
-			while (fakeStory = fakeStory.nextElementSibling) {
-				fakeStory.style.display = 'none';
-			}
-		};
-
-		hideFakeNews();
-
-		const observer = new MutationObserver(createObserver(hideFakeNews));
-		observer.observe(fakeNews, {childList: true})
+		return mutationCb;
 	}
-}
 
-const pid = setInterval(checker, 300);
 
+	/**
+	 * on Twitter.com: hide the "What's happening"
+	 * panel by default. A lot of fear-mongering
+	 * and garbage gets pushed here.
+	 *
+	 * Users can click the header to expand it
+	 * on demand.
+	 */
+	const checker = () => {
+		// safeguard in case user is not on main Twitter app
+		if (++n >= maxAttempts) clearInterval(pid);
+
+		const result = document.evaluate(`//span[text()="What’s happening"]/../../../../..`, document, null, 9, null);
+		const fakeNews = result.singleNodeValue;
+		if (fakeNews) {
+			clearInterval(pid);
+			// ready to work
+			const anchor = fakeNews.childNodes[1];
+			anchor.onclick = () => {
+				let fakeStory = anchor;
+				while (fakeStory = fakeStory.nextElementSibling) {
+					fakeStory.style.display = hidden ? '' : 'none';
+				}
+				hidden = !hidden;
+			};
+
+			const hideFakeNews = () => {
+				let fakeStory = anchor;
+				while (fakeStory = fakeStory.nextElementSibling) {
+					fakeStory.style.display = 'none';
+				}
+			};
+
+			hideFakeNews();
+
+			const observer = new MutationObserver(createObserver(hideFakeNews));
+			observer.observe(fakeNews.parentNode, {childList: true, subtree: true})
+		}
+	}
+
+
+	const pid = setInterval(checker, 300);
+};
+
+// https://stackoverflow.com/a/64927639/270302
+window.history.pushState = new Proxy(window.history.pushState, {
+	apply: (target, thisArg, argArray) => {
+		doIt();
+		return target.apply(thisArg, argArray);
+	},
+});
+
+
+doIt();
