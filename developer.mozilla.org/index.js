86224104565b670044b812fa8ed2ebc89433c2e9
avindra <avindra@users.noreply.github.com>
2021-08-07 05:13:35 +0000

    deploy: 53feacf8ea05a501240e05d9545123780d3f85cb

---
+++ developer.mozilla.org/index.js
@@ -0,0 +1,7 @@
+
+const s = document.createElement("link");
+s.type = 'text/css';
+s.rel = 'stylesheet';
+s.href = 'https://cdn.jsdelivr.net/gh/lndrcrvlh/MDN-after-dark@main/styles.css';
+
+document.body.appendChild(s);
---
+++ github.com/index.js
@@ -0,0 +1,20 @@
+
+const fileRegex = /\/([^/]+)\/([^/]+)\/blob\/([^]+)\/(.+)/;
+
+const fileMatch = location.pathname.match(fileRegex);
+
+if (fileMatch) {
+	const btns = document.querySelector(".BtnGroup");
+	if (btns) {
+		const [, user, repo, branch, file] = fileMatch;
+
+		const jsd = `https://cdn.jsdelivr.net/gh/${user}/${repo}@${branch}/${file}`;
+		const a = document.createElement('a');
+		a.href = jsd;
+		a.textContent = `JsDelivr`;
+		a.className = `btn-sm btn BtnGroup-item`;
+
+		btns.appendChild(a);
+	}
+
+}
