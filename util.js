2694b0d941d37df5409eb2422831e454af0019e9
avindra <avindra@users.noreply.github.com>
2021-08-07 03:33:14 +0000

    deploy: dd478b92b1228eb470a32fed3251928c00c5657b

---
+++ util.js
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ * @param {Function} doit Thing to run when user navigates
+ */
+export const onBrowse = (doit) => {
+	window.onpopstate = doit;
+
+	// https://stackoverflow.com/a/64927639/270302
+	['pushState', 'replaceState'].forEach(type => {
+		window.history[type] = new Proxy(window.history[type], {
+			apply: (target, thisarg, argarray) => {
+				doit();
+				return target.apply(thisarg, argarray);
+			},
+		});
+	});
+
+	doit();
+}
