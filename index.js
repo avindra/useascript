53feacf8ea05a501240e05d9545123780d3f85cb
Avindra Goolcharan <aavindraa@gmail.com>
2021-08-07 01:12:33 -0400

    GitHub: show jsdeliver URL for files
    Also stubbed mdn script

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const domain = location.host.replace('www.', '');
 
-switch(domain) {
-	case 'google.com':
-	case 'twitter.com':
-	case 'youtube.com':
-		await import(`./${domain}/index.js`);
-	default:
-		// nothing to do.
+let target;
+
+switch(location.host) {
+	case 'github.com':
+	case 'developer.mozilla.org': {
+		target = location.host;
+		break;
+	}
+	default: {
+		const domain = location.host.replace('www.', '');
+
+		switch(domain) {
+			case 'google.com':
+			case 'twitter.com':
+			case 'youtube.com':
+				target = domain;
+		}
+	}
+}
+
+
+if (target) {
+	await import(`./${target}/index.js`);
 }
