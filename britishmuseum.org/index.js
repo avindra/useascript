061189eee959f6959ea97ea970c9c4bd12199bde
avindra <avindra@users.noreply.github.com>
2021-08-23 20:32:31 +0000

    deploy: f237a18278195c9cdad7db21c289f4362615b466

---
+++ britishmuseum.org/index.js
@@ -0,0 +1,12 @@
+
+/**
+ * Repair legacy ASPX link by redirecting
+ * to search
+ */
+if (location.pathname === "/research/search_the_collection_database/search_object_details.aspx") {
+	const params = new URLSearchParams(location.search.substring(1));
+	const oid = params.get('objectId');
+	if (oid) {
+		location.href = `/collection/search?keyword=${oid}`;
+	}
+}
