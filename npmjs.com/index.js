813bf743079265503644ee7e4ab4955b78db76f2
Avindra Goolcharan <aavindraa@gmail.com>
2021-10-11 05:57:21 -0400

    npm: add packagephobia link

--- npmjs.com/index.js
+++ npmjs.com/index.js
@@ -4,6 +4,11 @@ if (pkg) {
 	const bp = document.createElement('a');
 	bp.href = `https://bundlephobia.com/package/${pkg.textContent}`;
 	bp.textContent = 'Bundlephobia';
-	pkg.append(bp);
+	pkg.parentNode.append(bp);
 
+
+	const pp = document.createElement('a');
+	pp.href = `https://packagephobia.com/result?p=${pkg.textContent}`;
+	pp.textContent = 'Packagephobia';
+	pkg.parentNode.append(pp);
 }
