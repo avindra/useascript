aff538e0baef924038c278fcb0f660e65f2fc011
Avindra Goolcharan <aavindraa@gmail.com>
2023-02-19 18:27:58 -0500

    rome fmt

--- index.js
+++ index.js
@@ -3,32 +3,30 @@ let target;
 switch (location.host) {
 	case "github.com":
 	case "twitter.com":
-	case "mobile.twitter.com":
-		{
-			/**
+	case "mobile.twitter.com": {
+		/**
 		 * Get root domain
 		 * @see https://stackoverflow.com/a/61381607/270302
 		 */
-			const rootDomain = location.host.split(".").slice(-2).join(".");
-			target = rootDomain;
-			break;
-		}
-	default:
-		{
-			const domain = location.host.replace("www.", "");
+		const rootDomain = location.host.split(".").slice(-2).join(".");
+		target = rootDomain;
+		break;
+	}
+	default: {
+		const domain = location.host.replace("www.", "");
 
-			switch (domain) {
-				case "britishmuseum.org":
-				case "holy-bhagavad-gita.org":
-				case "google.com":
-				case "ggpht.com":
-				case "guidestar.org":
-				case "npmjs.com":
-				case "reddit.com":
-				case "youtube.com":
-					target = domain;
-			}
+		switch (domain) {
+			case "britishmuseum.org":
+			case "holy-bhagavad-gita.org":
+			case "google.com":
+			case "ggpht.com":
+			case "guidestar.org":
+			case "npmjs.com":
+			case "reddit.com":
+			case "youtube.com":
+				target = domain;
 		}
+	}
 }
 
 if (target) {
