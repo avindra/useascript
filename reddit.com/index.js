663d2d197700aa577da19bf2ea6b71c7430536af
Avindra Goolcharan <aavindraa@gmail.com>
2023-02-20 20:02:25 -0500

    lint reddit script

--- reddit.com/index.js
+++ reddit.com/index.js
@@ -19,25 +19,23 @@ const targetNode = document.querySelector(".ListingLayout-outerContainer");
 const config = { attributes: true, childList: true, subtree: true };
 
 let runs = 0;
-const observer = new MutationObserver(
-	(mutations, _) => {
-		for (const mut of mutations) {
-			if (mut.target.tagName != "DIV") {
-				continue;
-			}
-
-			if (mut.type === "childList") {
-				run_it(mut.target);
-				console.log("ran it", ++runs, "times", mut);
-			}
+const observer = new MutationObserver((mutations, _) => {
+	for (const mut of mutations) {
+		if (mut.target.tagName !== "DIV") {
+			continue;
 		}
-	},
-);
+
+		if (mut.type === "childList") {
+			run_it(mut.target);
+			console.log("ran it", ++runs, "times", mut);
+		}
+	}
+});
 
 observer.observe(targetNode, config);
 
 /**
- * 
+ *
  */
 function run_it(node = document) {
 	if (node.marked) {
@@ -48,74 +46,74 @@ function run_it(node = document) {
 	const sections = location.pathname.split(/(?!^)\//);
 	const loc = sections[0];
 	if (["/user", "/r"].includes(loc)) {
-		[...node.querySelectorAll("a")].forEach(
-			(element) => {
-				const href = element.getAttribute("href");
-				if (!href) {
-					return;
+		[...node.querySelectorAll("a")].forEach((element) => {
+			const href = element.getAttribute("href");
+			if (!href) {
+				return;
+			}
+			if (element.title) {
+				if (href !== "/s") {
+					// prevent revealing spoilers
+					const tit = document.createElement("span");
+					tit.textContent = `(${element.title})`;
+					tit.className = "titText";
+					element.parentNode.insertBefore(tit, element);
 				}
-				if (element.title) {
-					if (href != "/s") {
-						// prevent revealing spoilers
-						var tit = document.createElement("span");
-						tit.textContent = "(" + element.title + ")";
-						tit.className = "titText";
-						element.parentNode.insertBefore(tit, element);
-					}
+			}
+			if (/\.(?:(jpe?|pn)g|gif|bmp)/.test(href)) {
+				element.style.color = "blue";
+				element.style.fontWeight = "bold";
+				if (href === element.textContent) {
+					element.textContent = "{Image}";
 				}
-				if (/\.(?:(jpe?|pn)g|gif|bmp)/.test(href)) {
-					element.style.color = "blue";
-					element.style.fontWeight = "bold";
-					if (href == element.textContent) {
-						element.textContent = "{Image}";
-					}
-				} else if (href.indexOf("/") == 0) {
-				} else if (/https?:\/\/(?:www\.)?reddit\.com/.test(href)) {
-					element.className += " favicon";
-					element.style.color = "crimson";
-					element.style.backgroundImage =
-						"url(\"http://www.reddit.com/static/favicon.ico\")";
-				} else if (/http:\/\/(?:www\.)?youtube\.com/.test(href)) {
-					element.className += " favicon";
-					element.style.color = "red";
-					element.style.backgroundImage =
-						"url(\"http://s.ytimg.com/yt/favicon.ico\")";
-					if (href == element.textContent) {
-						element.textContent = "{Video}";
-					}
-				} else if (/http:\/\/(?:www\.)?facebook\.com/.test(href)) {
-					if (href.match(/[&?]id=(\d+)/)) {
-						element.innerHTML = "Profile: {" + RegExp.$1 + "}";
-					} else if (element.pathname.match(/^\/([^?&#]+)/)) {
-						element.innerHTML = "Profile: " + RegExp.$1;
-					}
-					element.className += " favicon";
-					element.style.color = "darkBlue";
-					element.style.backgroundImage =
-						"url(http://slippyd.com/assets/70/logo.facebook.favicon.s16.png)";
-				} else if (/http:\/\/(?:www\.)?en\.wikipedia\.org\//.test(href)) {
-					if (/\/wiki\/([^\/]+)/.test(element.pathname)) {
-						if (element.textContent == href) {
-							element.innerHTML = "";
-						}
-						element.innerHTML +=
-							" <small>{" + unescape(RegExp.$1).replace(/_/g, " ") + "}</small>";
-					}
-					element.style.color = "black";
-					element.style.backgroundImage =
-						"url(\"http://en.wikipedia.org/favicon.ico\")";
-					element.className += " favicon";
-				} else if (
-					!element.href.startsWith(location.protocol + "//" + location.host)
-				) {
-					element.className += " favicon";
-					element.style.color = "darkGreen";
-					element.style.backgroundImage =
-						"url(\"https://upload.wikimedia.org/wikipedia/commons/0/0f/External-link-ltr-icon_Dark.png\")";
-					element.style.paddingRight = "11px";
+			} else if (href.indexOf("/") === 0) {
+			} else if (/https?:\/\/(?:www\.)?reddit\.com/.test(href)) {
+				element.className += " favicon";
+				element.style.color = "crimson";
+				element.style.backgroundImage =
+					'url("http://www.reddit.com/static/favicon.ico")';
+			} else if (/http:\/\/(?:www\.)?youtube\.com/.test(href)) {
+				element.className += " favicon";
+				element.style.color = "red";
+				element.style.backgroundImage =
+					'url("http://s.ytimg.com/yt/favicon.ico")';
+				if (href === element.textContent) {
+					element.textContent = "{Video}";
 				}
-			},
-		);
+			} else if (/http:\/\/(?:www\.)?facebook\.com/.test(href)) {
+				if (href.match(/[&?]id=(\d+)/)) {
+					element.innerHTML = `Profile: {${RegExp.$1}}`;
+				} else if (element.pathname.match(/^\/([^?&#]+)/)) {
+					element.innerHTML = `Profile: ${RegExp.$1}`;
+				}
+				element.className += " favicon";
+				element.style.color = "darkBlue";
+				element.style.backgroundImage =
+					"url(http://slippyd.com/assets/70/logo.facebook.favicon.s16.png)";
+			} else if (/http:\/\/(?:www\.)?en\.wikipedia\.org\//.test(href)) {
+				if (/\/wiki\/([^\/]+)/.test(element.pathname)) {
+					if (element.textContent === href) {
+						element.innerHTML = "";
+					}
+					element.innerHTML += ` <small>{${unescape(RegExp.$1).replace(
+						/_/g,
+						" ",
+					)}}</small>`;
+				}
+				element.style.color = "black";
+				element.style.backgroundImage =
+					'url("http://en.wikipedia.org/favicon.ico")';
+				element.className += " favicon";
+			} else if (
+				!element.href.startsWith(`${location.protocol}//${location.host}`)
+			) {
+				element.className += " favicon";
+				element.style.color = "darkGreen";
+				element.style.backgroundImage =
+					'url("https://upload.wikimedia.org/wikipedia/commons/0/0f/External-link-ltr-icon_Dark.png")';
+				element.style.paddingRight = "11px";
+			}
+		});
 	}
 	node.marked = true;
 }
