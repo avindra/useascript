b4350a0971889d9627751edc4b260ecbfdfad12b
avindra <avindra@users.noreply.github.com>
2024-02-28 03:08:19 +0000

    deploy: 0003b51051ed957bae675ea3871326e78acdc6d6

--- ggpht.com/index.js
+++ ggpht.com/index.js
@@ -1,7 +1,7 @@
-const S = /(yt\d+.(?:ggpht|googleusercontent).com.+)s\d+-c/;
+const S = /s\d+-([cp])/;
 
 // default size to 4096px
 if (!location.href.includes("4096")) {
-	const dest = location.href.replace(S, "$1-s4096-c");
+	const dest = location.href.replace(S, "s4096-$1");
 	location.href = dest;
 }
