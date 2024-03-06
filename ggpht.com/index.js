68c0f325493f8f0385d9eca59c85553a5c0264a0
avindra <avindra@users.noreply.github.com>
2024-03-06 17:53:09 +0000

    deploy: 538ec75533738e90e9775bfb64f0f14c61d06e7b

--- ggpht.com/index.js
+++ ggpht.com/index.js
@@ -3,5 +3,12 @@ const S = /s\d+-([cp])/;
 // default size to 4096px
 if (!location.href.includes("4096")) {
 	const dest = location.href.replace(S, "s4096-$1");
-	location.href = dest;
+	if (dest !== location.href) {
+		location.href = dest;
+	}
+
+	const dest2 = location.href.replace(/w\d+-h\d+/, "w4096");
+	if (dest2 !== location.href) {
+		location.href = dest2;
+	}
 }
