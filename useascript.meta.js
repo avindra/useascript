58a94d9213873401357618e29705bb4b808e57d9
Avindra Goolcharan <aavindraa@gmail.com>
2026-02-26 18:17:14 -0500

    bundle using CICD (#9)
    * merge test and deploy workflows
    * setup *.meta.js file for use with updateURL

---
+++ useascript.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         useascript
+// @namespace    https://dra.vin/
+// @version      0.6
+// @description  A compendium of userscripts for the Web
+// @author       Avindra Goolcharan
+// @match        *://*/*
+// @updateURL    https://dra.vin/useascript/useascript.meta.js
+// ==/UserScript==
