52220a5322a5e5377dd4686cb4d0ab17200efcef
avindra <avindra@users.noreply.github.com>
2026-02-26 22:50:28 +0000

    deploy: c91d19bfbcb1f3f02f2790dd616e72f68a5fb859

--- hook.user.js
+++ hook.user.js
@@ -1,21 +1,10 @@
 // ==UserScript==
-// @name         useascript-hook
+// @name         useascript
 // @namespace    https://dra.vin/
 // @version      0.1
-// @description  Loader for useascript, a compendium of userscripts for the Web
+// @description  A compendium of userscripts for the Web
 // @author       Avindra Goolcharan
 // @match        *://*/*
 // @grant        none
+// @require      https://dra.vin/useascript/dist/index.js
 // ==/UserScript==
-
-/**
- *
- * @param {Document} d A document to work on
- */
-((d) => {
-	const s = d.createElement("script");
-	s.type = "module";
-	s.src = "https://dra.vin/useascript/index.js";
-
-	d.body.appendChild(s);
-})(document);
