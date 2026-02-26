f6a317f92b29a9458d5d710913e0d640a1b5cb1f
Avindra Goolcharan <aavindraa@gmail.com>
2026-02-26 18:08:08 -0500

    bundle preamble in ci

---
+++ useascript.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         useascript
+// @namespace    https://dra.vin/
+// @version      0.2
+// @description  A compendium of userscripts for the Web
+// @author       Avindra Goolcharan
+// @match        *://*/*
+// @updateURL    https://dra.vin/useascript/useascript.meta.js
+// ==/UserScript==
