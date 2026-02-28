93d6dd7c92bb28b21cb71f34cb7ae71eecdde341
Avindra Goolcharan <aavindraa@gmail.com>
2026-02-27 20:23:04 -0500

    X: stylize script button

--- x.com/sidebar.js
+++ x.com/sidebar.js
@@ -5,13 +5,29 @@ import { sleep } from "../util.js";
  */
 export const setup = async (callback) => {
 	await sleep(500);
+
+	// copy an existing button to use as a template for styling
 	const root = document.querySelector("header nav");
 	const link = root.firstChild;
 	const button = link.cloneNode(true);
+
+	// setup scroll icon
+	const scroll = document.createTextNode("s");
+	scroll.textContent = "📜";
+	const icon = button.firstChild.firstChild;
+	icon.replaceWith(scroll);
+
+	scroll.parentNode.style.fontSize = "30px";
+
 	const label = button.querySelector("span");
+	Object.assign(label.style, {
+		fontFamily: "monospace",
+		fontStyle: "italic",
+		fontSize: "smaller",
+	});
 	if (label) {
 		// label is hidden at smaller resolutions
-		label.textContent = "Script";
+		label.textContent = "Useascript";
 	}
 	button.removeAttribute("href");
 	button.onclick = callback;
